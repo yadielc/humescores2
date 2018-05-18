efb2e8a20519ef8dc490b98ac5232abfeda346aa
Yadiel <ysites@gmail.com>
2018-05-18 15:03:37 -0400

    added resource hints to functions.php file

--- functions.php
+++ functions.php
@@ -115,6 +115,28 @@ function humescores2_fonts_url() {
 }
 
 /**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function humescores2_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'humescores2-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'humescores2_resource_hints', 10, 2 );
+
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
